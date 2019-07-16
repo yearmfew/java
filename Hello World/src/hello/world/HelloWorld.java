5a7d24f7d69759db14e9c8b3bfe7f19e4fe83ed8
yearmfew <brlylmz23@gmail.com>
2019-07-16 19:30:20 +0200

    terminalden java dosyası çalıştırmak için bir deneme yaptık. Ancak java sürümünden dolayı yapamadık. Javac ile class oluştururken java 11 e göre oluşturuyor ama dosya java 8 ile uyumlu. Bu tarz bir hatadan dolayı dosyayı çalıştıramadım.

--- Hello World/src/hello/world/HelloWorld.java
+++ Hello World/src/hello/world/HelloWorld.java
@@ -17,6 +17,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello world");
+        System.out.println("haha");
+        System.out.println("hellooooo");
     }
     
 }
